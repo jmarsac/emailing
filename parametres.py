79e0a0eb3b756b842c57518e4706adc543648b1a
Jean-Marie Arsac <jmarsac@azimut.fr>
2018-10-11 22:58:30 +0200

    Sollicitation sponsors

--- parametres.py
+++ parametres.py
@@ -10,7 +10,7 @@ def InitParams():
    #
    # contenu du mail
    #
-   params["Sujet"] = "OSGeo-fr"
+   params["Sujet"] = "Sponsorisation FOSS4G-fr 2018"
    # emplacement du fichier logo inséré après la signature
    params["Logo"] = "logo\\osgeo.fr.svg-222x85.png"
    #
@@ -43,24 +43,48 @@ def InitParams():
    # ATTENTION: à respecter l'ordre Prénom,Nom ou Nom,Prénom dans le fichier .csv
    params["TextBody"] = '''Bonjour $1,
 
-Vous êtes géomaticien ou intéressé par l'information géographique.
-Venez nous rejoindre <https://www.osgeo.asso.fr/content/lassociation>
 
-Merci par avance de votre adhésion !
+L'édition 2018 du FOSS4G-fr aura lieu du 15 au 17 mai dans les locaux de l'ENSG (École Nationale des Sciences Géographiques), à Marne-la-Vallée (accès depuis Paris par le RER A).<br />
 
-Le bureau de l'association
+
+Organisé par l'OSGeo-fr, cet événement est dédié à la Géomatique Open Source et aux données géographiques libres.<br />
+Les précédentes éditions ont rencontré un réel succès, et réuni lors de la dernière édition près de 250 utilisateurs, développeurs et professionnels des SIG (Systèmes d'Information Géographique), pour une journée d'ateliers et deux jours de conférences.
+
+
+Sur un format similaire, le comité d'organisation se donne, cette année,  comme ambition de réunir un public encore plus large de participants et conférenciers, issus de la communauté Open Source Geospatial francophone.
+L'OSGeo-fr, association organisatrice de l'événement, recherche des sponsors pour financer cette rencontre.<br />
+C'est "l'Evénement biennal de la Géomatique Open Source" qui permet aux participants d'assurer leur veille technologique.<br />
+En tant qu'acteur de la Géomatique Open Source, soutenir l'événement vous permettra d'améliorer votre visibilité.<br />
+
+Les trois niveaux de sponsorisation sont accessibles <a href="http://osgeo.asso.fr/foss4gfr-2018/sponsors.html#appel-sponsors">ici</a><br />
+
+
+Espérant pouvoir compter sur votre soutien, nous restons à votre disposition pour tout complément d'information et remercions par avance $1 pour sa participation.<br />
+Meilleures salutations.
+
+
+L'équipe organisatrice du @FOSS4Gfr
+
 '''
 
    # corps du mail en HTML
    # ATTENTION: à respecter l'ordre Prénom,Nom ou Nom,Prénom dans le fichier .csv
-   params["HtmlBody"] = '''Bonjour $1,
-<br /><br />
-Vous êtes géomaticien ou intéressé par l'information géographique.
-<a href="https://www.osgeo.asso.fr/content/lassociation">Venez nous rejoindre</a> 
-<br /><br />
-Merci par avance de votre adhésion !
-<br /><br />
-Le bureau de l'association
+   params["HtmlBody"] = '''Bonjour $2,
+<br /><br /><br />
+L'édition 2018 du FOSS4G-fr aura lieu du 15 au 17 mai dans les locaux de l'ENSG (École Nationale des Sciences Géographiques), à Marne-la-Vallée (accès depuis Paris par le RER A).<br />
+<br />
+Organisé par l'OSGeo-fr, cet événement est dédié à la Géomatique Open Source et aux données géographiques libres.<br />
+Les précédentes éditions ont rencontré un réel succès, et réuni lors de la dernière édition près de 250 utilisateurs, développeurs et professionnels des SIG (Systèmes d'Information Géographique), pour une journée d'ateliers et deux jours de conférences.
+<br />
+Sur un format similaire, le comité d'organisation se donne, cette année,  comme ambition de réunir un public encore plus large de participants et conférenciers, issus de la communauté Open Source Geospatial francophone.
+L'OSGeo-fr, association organisatrice de l'événement, recherche des sponsors pour financer cette rencontre.<br />
+C'est "l'Evénement biennal de la Géomatique Open Source" qui permet aux participants d'assurer leur veille technologique.<br />
+En tant qu'acteur de la Géomatique Open Source, soutenir l'événement vous permettra d'améliorer votre visibilité.<br />
+<br />
+Les trois niveaux de sponsorisation sont accessibles <a href="http://osgeo.asso.fr/foss4gfr-2018/sponsors.html#appel-sponsors">ici</a><br />
+<br />
+Espérant pouvoir compter sur votre soutien, nous restons à votre disposition pour tout complément d'information et remercions par avance $1 pour sa participation.<br />
+Meilleures salutations.
 
 '''
    return params
