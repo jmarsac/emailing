e4f4a5740dc5457530ae8735ee81659e20ba25b8
Jean-Marie Arsac <jmarsac@azimut.fr>
2018-10-11 23:01:39 +0200

    Appel de cotisation pour élections bureau 2018

--- parametres.py
+++ parametres.py
@@ -10,7 +10,7 @@ def InitParams():
    #
    # contenu du mail
    #
-   params["Sujet"] = "OSGeo-fr"
+   params["Sujet"] = "Elections bureau"
    # emplacement du fichier logo inséré après la signature
    params["Logo"] = "logo\\osgeo.fr.svg-222x85.png"
    #
@@ -43,25 +43,56 @@ def InitParams():
    # ATTENTION: à respecter l'ordre Prénom,Nom ou Nom,Prénom dans le fichier .csv
    params["TextBody"] = '''Bonjour $1,
 
-Vous êtes géomaticien ou intéressé par l'information géographique.
-Venez nous rejoindre <https://www.osgeo.asso.fr/content/lassociation>
+La clôture des listes électorales aura lieu dimanche 7 octobre à minuit. Tu n'es pour l'instant pas à jour de ta cotisation 2018.
+Ce mail n'est pas destiné à faire du forcing mais simplement te permettre de voter pour élire le bureau si tu le désires.
 
-Merci par avance de votre adhésion !
+Si tu ne veux pas renouveler ton adhésion, désolé pour le dérangement et bon week-end; si c'est un oubli, la procédure est indiquée ci-après...
 
-Le bureau de l'association
+Cordialement.
+
+Jean-Marie
+
+*PS: merci de nous indiquer si tu souhaites ne plus recevoir de communication de la part de l'association et résilier ton adhésion*
+
+
+Pour te connecter sur Dolibarr, ton login est "$2" :
+
+https://www.osgeo.asso.fr > ASSOCIATION > ESPACE ADHERENT
+
+Concernant le règlement de ta cotisation, le lien Paypal n'est pas toujours opérationnel, dans ce cas, tu peux utiliser HelloAsso : <https://www.helloasso.com/associations/osgeo-fr/adhesions/adhesion-renouvellement-osgeo-fr>
+
+Merci d'avance.
+
+Bon week-end.
+
+
 '''
 
    # corps du mail en HTML
    # ATTENTION: à respecter l'ordre Prénom,Nom ou Nom,Prénom dans le fichier .csv
    params["HtmlBody"] = '''Bonjour $1,
 <br /><br />
-Vous êtes géomaticien ou intéressé par l'information géographique.
-<a href="https://www.osgeo.asso.fr/content/lassociation">Venez nous rejoindre</a> 
+La clôture des listes électorales aura lieu dimanche 7 octobre à minuit. Tu n'es pour l'instant pas à jour de ta cotisation 2018.
+Ce mail n'est pas destiné à faire du forcing mais simplement te permettre de voter pour élire le bureau si tu le désires.
 <br /><br />
-Merci par avance de votre adhésion !
+Si tu ne veux pas renouveler ton adhésion, désolé pour le dérangement et bon week-end; si c'est un oubli, la procédure est indiquée ci-après...
 <br /><br />
-Le bureau de l'association
-
+Cordialement.
+<br /><br />
+Jean-Marie
+<br /><br />
+<strong>PS: merci de nous indiquer si tu souhaites ne plus recevoir de communication de la part de l'association et résilier ton adhésion</strong>
+<br /><br />
+Pour te connecter sur Dolibarr, ton login est "$2" :
+<br /><br />
+https://www.osgeo.asso.fr > ASSOCIATION > <a href="http://osgeo.asso.fr/dolibarr/htdocs/">ESPACE ADHERENT</a> 
+<br /><br />
+Concernant le règlement de ta cotisation, le lien Paypal n'est pas toujours opérationnel, dans ce cas, tu peux utiliser <a href="https://www.helloasso.com/associations/osgeo-fr/adhesions/adhesion-renouvellement-osgeo-fr">HelloAsso</a>
+<br /><br />
+Merci d'avance.
+<br />
+Bon week-end.
+<br /><br />
 '''
    return params
 
